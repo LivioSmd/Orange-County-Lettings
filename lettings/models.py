90d625e8565e6f67b1ae5e4dd779fd8f399b4d40
Livio Simard <liviosimard@yahoo.fr>
2025-06-06 02:34:30 +0200

    flake8 correction

--- lettings/models.py
+++ lettings/models.py
@@ -20,5 +20,3 @@ class Letting(models.Model):
 
     def __str__(self):
         return self.title
-
-
--- oc_lettings_site/models.py
+++
@@ -1,3 +0,0 @@
-# oc_lettings_site/models.py
-
-# This file is now empty because the models have been moved to 'lettings' and 'profiles' apps.
